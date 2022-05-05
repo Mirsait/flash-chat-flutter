0947267f9584036df381933c9c86b84fa01253a4
Mirsait <mavcharenko@gmail.com>
2022-05-05 22:32:14 +0300

    setup routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
 
 void main() => runApp(const FlashChat());
 
@@ -9,12 +12,18 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.black54),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Colors.black54),
+          ),
         ),
-      ),
-      home: const WelcomeScreen(),
-    );
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => const WelcomeScreen(),
+          LoginScreen.id: (context) => const LoginScreen(),
+          RegistrationScreen.id: (context) => const RegistrationScreen(),
+          ChatScreen.id: (context) => const ChatScreen(),
+        });
   }
 }
